a4e04d4a700da694e094900a597708a1ce9cec06
Thuwayba15 <u22622668@tuks.co.za>
2024-10-01 14:25:54 +0200

    State: Locked and unlocked

--- LockedState.cpp
+++ LockedState.cpp
@@ -1,16 +1,24 @@
 #include <exception>
 #include <string>
+#include <iostream>
 using namespace std;
 
 #include "LockedState.h"
 #include "SmartDevice.h"
 #include "DeviceState.h"
+#include "UnlockedState.h"
 
 string LockedState::getStatus() {
-	throw "Not yet implemented";
+	return "Locked";
 }
 
-void LockedState::performAction(SmartDevice aDevice, string aAction) {
-	throw "Not yet implemented";
+void LockedState::performAction(SmartDevice& device, string action) {
+	if (action == "Unlock") {
+        device.setState(new UnlockedState());
+    } else if (action == "Lock") {
+        std::cout<<"Device is already locked"<<std::endl;
+    } else {
+
+    }
 }
 
--- UnlockedState.cpp
+++ UnlockedState.cpp
@@ -1,16 +1,24 @@
 #include <exception>
 #include <string>
+#include <iostream>
 using namespace std;
 
 #include "UnlockedState.h"
 #include "SmartDevice.h"
 #include "DeviceState.h"
+#include "LockedState.h"
 
 string UnlockedState::getStatus() {
 	throw "Not yet implemented";
 }
 
-void UnlockedState::performAction(SmartDevice device, string action) {
-	throw "Not yet implemented";
+void UnlockedState::performAction(SmartDevice& device, string action) {
+	if (action == "Lock") {
+        device.setState(new LockedState());
+    } else if (action == "Unlock") {
+        std::cout<<"Device is already unlocked"<<std::endl;
+    } else {
+
+    }
 }
 
