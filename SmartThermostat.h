c1cf542e08d9ebd1f0041e7f89fac6a6232a03eb
bahiya6 <u22598546@tuks.co.za>
2024-10-05 17:20:23 +0200

    Add files via upload

--- SmartThermostat.h
+++ SmartThermostat.h
@@ -1,15 +1,17 @@
 
 #ifndef __SmartThermostat_h__
 #define __SmartThermostat_h__
+#include "SmartDevice.h"
 
 class SmartThermostat;
 
-class SmartThermostat
+class SmartThermostat : public SmartDevice 
 {
 	private: 
 		int temperature;
 
 	public: 
+		SmartThermostat();
 		virtual void setTemperature(int temp) = 0;
 		virtual int getTemperature() = 0;
 };
